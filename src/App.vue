2c6d09f2507d637e8f5a3290d08d394a1608def0
Sheng Qing <qing.sheng@thomsonreuters.com>
2017-10-13 16:05:54 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <tr-header></tr-header>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import TrHeader from './components/GvHeader'
+
+export default {
+  name: 'app',
+  components: {
+    TrHeader,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  height: 100%;
+}
+body, html {
+  margin: 0;
+  background-color: #000;
+  height: 100%;
+}
+</style>
---
+++ src/components/GvHeader.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <div class="gv_logo">
+      <img src="../assets/logo.png"/>
+    </div>
+    <div class="gv_time_style">
+    <span id="gv_date" class="gv_date">
+      {{date.toLocaleDateString()}}
+    </span><br>
+      <span id="gv_time" class="gv_time">
+      {{date.toLocaleTimeString('en-us', options)}}
+    </span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TrHeader',
+    data () {
+      const options = {
+        hour: '2-digit', minute: '2-digit'//, second: '2-digit'
+      }
+      return {
+        options: options,
+        date: new Date()
+      }
+    },
+    created: function () {
+      // `this` 指向 vm 实例
+      this.interval = setInterval(() => {
+        this.date = new Date()
+      }, 1000)
+    },
+    destroyed: function () {
+      clearInterval(this.interval)
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  * {
+    font-family: 'KnowledgeMedium';
+  }
+
+  .container {
+    background-color: rgb(37, 37, 41);
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .gv_date {
+    font-weight: bold;
+    font-size: 30px;
+  }
+
+  .gv_time {
+    font-weight: bold;
+    font-size: 25px;
+  }
+</style>
